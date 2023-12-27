f8e3935d0272a71e82545b0b91471278a369d75d
Jeremy Harris <jeremy.harris@zenosmosis.com>
2023-12-26 22:10:05 -0600

    Extract script into separate file

---
+++ static/scripts/main.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('menu-toggle-button');
+  var tocContainer = document.querySelector('.toc-container');
+
+  if (button && tocContainer) {
+    // Hide the button by default
+    button.style.display = 'none';
+
+    button.addEventListener('click', function() {
+      tocContainer.style.display = (tocContainer.style.display === 'none' || tocContainer.style.display === '') ? 'block' : 'none';
+    });
+
+    // Show the button when JavaScript is enabled
+    button.style.display = 'block';
+
+    // Hide the TOC by default on mobile
+    if (window.innerWidth <= 768) {
+      tocContainer.style.display = 'none';
+    }
+  }
+});
