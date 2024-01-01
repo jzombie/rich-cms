487abe92583eaa80eb06d72d2ab79b7240fac363
Jeremy Harris <jeremy.harris@zenosmosis.com>
2024-01-01 15:19:30 -0600

    Fix issue where clicking hamburger button a single time would not hide menu

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,21 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var button = document.getElementById('menu-toggle-button');
-  var tocContainer = document.querySelector('.toc-container');
+document.addEventListener("DOMContentLoaded", function () {
+  var button = document.getElementById("menu-toggle-button");
+  var tocContainer = document.querySelector(".toc-container");
 
   if (button && tocContainer) {
     // Hide the button by default
-    button.style.display = 'none';
+    button.style.display = "none";
 
-    button.addEventListener('click', function() {
-      tocContainer.style.display = (tocContainer.style.display === 'none' || tocContainer.style.display === '') ? 'block' : 'none';
+    button.addEventListener("click", function () {
+      tocContainer.style.display =
+        tocContainer.style.display === "none" ? "block" : "none";
     });
 
     // Show the button when JavaScript is enabled
-    button.style.display = 'block';
+    button.style.display = "block";
 
     // Hide the TOC by default on mobile
     if (window.innerWidth <= 768) {
-      tocContainer.style.display = 'none';
+      tocContainer.style.display = "none";
     }
   }
 });
